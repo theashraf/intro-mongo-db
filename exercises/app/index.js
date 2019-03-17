dddea5eac73567ac14642070240af8ca75e87515
Abdelrahman Ashraf <a.theashraf@gmail.com>
2019-03-17 16:40:26 +0200

    add app solution

--- exercises/app/index.js
+++ exercises/app/index.js
@@ -1,28 +1,39 @@
-const express = require('express')
-const morgan = require('morgan')
-const connect = require('../connect')
-const {json, urlencoded} = require('body-parser')
-const app = express()
-const Todo = require('./todo')
+const express = require("express");
+const morgan = require("morgan");
+const connect = require("../connect");
+const { json, urlencoded } = require("body-parser");
+const app = express();
+const Todo = require("./todo");
 
-app.use(morgan('dev'))
-app.use(urlencoded({extended: true}))
-app.use(json())
+app.use(morgan("dev"));
+app.use(urlencoded({ extended: true }));
+app.use(json());
 
-app.get('/todo/:id', async (req, res) => {
-  const todoId = req.params.id
-})
+app.get("/todo/:id", async (req, res) => {
+  const todoId = req.params.id;
+  const todo = await Todo.findById(todoId)
+    .lean()
+    .exec();
+  res.status(200).json(todo);
+});
 
-app.get('/todos', async (req, res) => {
+app.get("/todos", async (req, res) => {
+  const todos = await Todo.find({})
+    .lean()
+    .exec();
+  res.status(200).json(todos);
+});
 
-})
+app.post("/todo", async () => {
+  const todoToCreate = req.body.todo;
+  const todo = await Todo.create(todoToCreate);
+  res.status(201).json(todo.toJSON());
+});
 
-app.post('/todo', async () => {
-  const todoToCreate = req.body.todo
-})
-
-connect(/**add mongo url here */)
-  .then(() => app.listen(4000, () => {
-    console.log('server on http://localhost:4000')
-  }))
-  .catch(e => console.error(e))
+connect("mongodb://localhost:27017/todos")
+  .then(() =>
+    app.listen(4000, () => {
+      console.log("server on http://localhost:4000");
+    })
+  )
+  .catch(e => console.error(e));
