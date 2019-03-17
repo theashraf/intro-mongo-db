fdfcb0b6fd1435cf1ec6959695e68ef55dfa6315
Abdelrahman Ashraf <a.theashraf@gmail.com>
2019-03-17 08:40:58 +0200

    add queries solution

--- exercises/queries/queries.js
+++ exercises/queries/queries.js
@@ -1,28 +1,38 @@
-const Post = require('./post')
+const Post = require("./post");
 
-const postByTitle = (title) => {
+const postByTitle = title => {
+  return Post.findOne({ title }).exec();
+};
 
-}
+const postsForAuthor = authorId => {
+  return Post.find({ author: authorId }).exec();
+};
 
-const postsForAuthor = (authorId) => {
+const fullPostById = id => {
+  return Post.findById(id)
+    .populate("author,similarPosts")
+    .exec();
+};
 
-}
-
-const fullPostById = (id) => {
-  
-}
-
-const allPostsSlim = (fieldsToSelect) => {
-  
-}
+const allPostsSlim = fieldsToSelect => {
+  return Post.find({})
+    .select(fieldsToSelect)
+    .exec();
+};
 
 const postByContentLength = (maxContentLength, minContentLength) => {
-  
-}
+  return Post.find({
+    contentLength: { $lt: maxContentLength, $gt: minContentLength }
+  }).exec();
+};
 
 const addSimilarPosts = (postId, similarPosts) => {
-  
-}
+  return Post.findByIdAndUpdate(
+    postId,
+    { $push: { similarPosts: { $each: similarPosts } } },
+    { new: true }
+  ).exec();
+};
 
 module.exports = {
   postByTitle,
@@ -31,4 +41,4 @@ module.exports = {
   allPostsSlim,
   postByContentLength,
   addSimilarPosts
-}
+};
