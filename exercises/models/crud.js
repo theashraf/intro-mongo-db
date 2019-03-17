6564972b4d4a1907728fce3131ee81af638d756a
Abdelrahman Ashraf <a.theashraf@gmail.com>
2019-03-17 05:42:51 +0200

    models exercise solution

--- exercises/models/crud.js
+++ exercises/models/crud.js
@@ -1,23 +1,23 @@
-const User = require('./user')
+const User = require("./user");
 
-const getUserById = (id) => {
- 
-}
+const getUserById = id => {
+  return User.findById(id).exec();
+};
 
 const getAllUsers = () => {
-  
-}
+  return User.find({}).exec();
+};
 
-const createUser = (userDetails) => {
-  
-}
-const removeUserById = (id) => {
-  
-}
+const createUser = userDetails => {
+  return User.create(userDetails);
+};
+const removeUserById = id => {
+  return User.findByIdAndRemove(id).exec();
+};
 
 const updateUserById = (id, update) => {
-
-}
+  return User.findByIdAndUpdate(id, update, { new: true }).exec();
+};
 
 module.exports = {
   getUserById,
@@ -25,4 +25,4 @@ module.exports = {
   createUser,
   removeUserById,
   updateUserById
-}
+};
